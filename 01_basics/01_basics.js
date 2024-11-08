82b0c63b2188b3a99545239176069721c9d996e6
Adiii2003 <153512569+Adiii2003@users.noreply.github.com>
2024-11-08 18:53:57 +0000

    container config

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,15 @@
+const accountId=122245  //memory name accountid  //const maii hmm change nhi kr sakte
+let accountemail="adityazxcv6@gmail.com" //scope wala problem nhi aata
+var accountpassword="12345"
+accountcity="jaipur" //bina variable ke kuch aage linkte hue hmm variable ki memory reserve kr skte haii
+// accountId=2  //not allowed
+accountemail="hc@gamil.com"
+let accountstate;
+
+accountpassword="123"
+accountcity="delhi"
+console.log(accountId);
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
+/* prefer not to use var
+beacuse of issue in block scope and functionl scope
+*/
